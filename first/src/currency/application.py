a3dc49e94dd5e648e36d17f0535daea3c2c80023
hainguyen114 <nguyenhoanghai110497@gmail.com>
2020-05-01 20:26:09 +0700

    update currency

--- first/src/currency/application.py
+++ first/src/currency/application.py
@@ -8,5 +8,21 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-# @app.route("/convert", method=["POST"])
-# def convert()
+@app.route("/convert", methods=["POST"])
+def convert():
+
+    # Query for currency exchange rate
+    currency = request.form.get("currency")
+    res = requests.get("https://api.fixer.io/latest", params={
+        "base": "USD", "symbols": currency})
+
+    # Make sure request succeeded
+    if res.status_code != 200:
+        return jsonify({"success": False})
+    
+    # Make sure currency is in response
+    data = res.json()
+    if currency not in data["rates"]:
+        return jsonify({"success": False})
+
+    return jsonify({"success": True, "rate": data["rates"][currency]})
